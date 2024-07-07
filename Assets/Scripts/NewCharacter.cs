6bde1aa96add6b66b9fead75a25b0a1a68ab232d
GraphicEdit <contact@graphicedit.com>
2024-07-07 14:27:28 +0100

    new chars

---
+++ Assets/Scripts/NewCharacter.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Character", menuName = "New Character", order = 1)]
+
+public class NewCharacter : ScriptableObject
+{
+    public Sprite characterPhoto;
+    public string characterName;
+    public GameObject characterPrefab;
+}
